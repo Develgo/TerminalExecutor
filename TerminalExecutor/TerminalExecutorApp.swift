9a6906b7aff3548f494f11dd9b879f69e0f5affa
Nitesh Maharaj <nitesh.maharaj@develgo.com>
2025-04-29 20:36:21 +0200

    Initial Commit

---
+++ TerminalExecutor/TerminalExecutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalExecutorApp.swift
+//  TerminalExecutor
+//
+//  Created by Nitesh Maharaj on 2025/04/29.
+//
+
+import SwiftUI
+
+@main
+struct TerminalExecutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
